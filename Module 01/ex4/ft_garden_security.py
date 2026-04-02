8aea703014e263c256117029b49c8a7613d3638a
Betania Pavlowski De Quevedo <bpavlows@c2r6s7.42porto.com>
2026-04-02 17:18:59 +0100

    ex 4 e 5

--- Module 01/ex4/ft_garden_security.py
+++ Module 01/ex4/ft_garden_security.py
@@ -7,23 +7,32 @@ class Plant:
         self.age = age
 
     def show(self) -> None:
-        print(f"Plant created: {self.name}: {self.height:,.1f}cm, {self.age} days old")
-    
+        print(
+            f"Plant created: {self.name}: {self.get_height():,.1f}cm,"
+            f"{self.get_age()} days old"
+        )
+
     def set_height(self, value: float) -> None:
         if (value <= 0):
             print(f"{self.name}: Error, height can't be negative")
             print("Height update rejected")
         else:
-            print(f"Height updated: {self.height:,.1f}cm")
             self.height = value
-    
+            print(f"Height updated: {self.height:,.1f}cm")
+
     def set_age(self, value: int) -> None:
         if (value <= 0):
             print(f"{self.name}: Error, age can't be negative")
             print("Age update rejected")
         else:
-            print(f"Age updated: {self.age}cm")
             self.age = value
+            print(f"Age updated: {self.age}cm")
+
+    def get_height(self) -> float:
+        return (self.height)
+
+    def get_age(self) -> int:
+        return (self.age)
 
 
 def main() -> None:
@@ -32,28 +41,17 @@ def main() -> None:
     print("=== Garden Security System ===")
     p1.show()
     print()
-    p1.set_height(25.0);
+    p1.set_height(25.0)
     p1.set_age(30)
     print()
-    p1.set_
-    Rose: Error, height can't be negative
-Height update rejected
-Rose: Error, age can't be negative
-Age update rejected
-Current state: Rose: 25.0cm, 30 days old
+    p1.set_height(-5)
+    p1.set_age(-5)
+    print()
+    print(
+        f"Current state: {p1.name}: {p1.get_height():,.1f}cm,"
+        f"{p1.get_age()} days old"
+    )
 
 
-    """
-    get_height(), get_age()
-
-
-Height updated: 25cm
-Age updated: 30 days
-
-Rose: Error, height can't be negative
-Height update rejected
-Rose: Error, age can't be negative
-Age update rejected
-
-Current state: Rose: 25.0cm, 30 days old
-    """
+if __name__ == "__main__":
+    main()
