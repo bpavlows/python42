b5220a91516df64e0477b9d0c1d5dcbe630cc56e
Betania Pavlowski De Quevedo <bpavlows@c2r1s8.42porto.com>
2026-04-20 12:50:40 +0100

    42

---
+++ Module 02/ex3/ft_custom_errors.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+class GardenError(Exception):
+    def __init__(self, message: str = "Unknown garden error"):
+        super().__init__(message)
+
+
+class PlantError(GardenError):
+    def __init__(self, message: str = "Unknown plant error"):
+        super().__init__(message)
+
+
+class WaterError(GardenError):
+    def __init__(self, message: str = "Unknown water error"):
+        super().__init__(message)
+
+
+def main() -> None:
+    main()
+
+
+if __name__ == "__main__":
+    main()
