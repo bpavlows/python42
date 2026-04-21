cf54cf2a997efa1597a456ab35c22764337a717a
Betania Pavlowski De Quevedo <bpavlows@c1r15s1.42porto.com>
2026-04-21 13:21:23 +0100

    change ex4

--- Module 02/ex4/ft_finally_block.py
+++ Module 02/ex4/ft_finally_block.py
@@ -10,14 +10,14 @@ class PlantError(GardenError):
         super().__init__(message)
 
 
-def water_plant(plant_name) -> None:
+def water_plant(plant_name: str) -> None:
     if (plant_name != plant_name.capitalize()):
         raise PlantError(f"Invalid plant name to water: {plant_name}")
     else:
         print(f"Watering {plant_name}: [OK]")
 
 
-def test_watering_system(plant_list) -> None:
+def test_watering_system(plant_list: list[str]) -> None:
     print("Opening watering system")
     try:
         for plant in plant_list:
