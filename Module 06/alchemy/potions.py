3dd07861bd34c5cedc1005c08ed78fbcb00720e4
Betania Pavlowski De Quevedo <bpavlows@c2r1s6.42porto.com>
2026-06-01 13:37:42 +0100

    part I ok and init part II

---
+++ Module 06/alchemy/potions.py
@@ -0,0 +1,12 @@
+from elements import create_fire
+from elements import create_water
+from .elements import create_earth
+from .elements import create_air
+
+
+def healing_potion() -> str:
+  return f"Healing potion brewed with '{create_earth()}' and '{create_air()}'"
+
+
+def strength_potion() -> str:
+  return f"Strength potion brewed with '{create_fire()}' and '{create_water()}'"
---
+++ Module 06/ft_distillation_0.py
@@ -0,0 +1,8 @@
+from alchemy.potions import healing_potion
+from alchemy.potions import strength_potion
+
+print("=== Distillation 0 ===")
+print("Direct access to alchemy/potions.py")
+print(f"Testing strength_potion: {strenght_potion()}")
+print(f"Testing healing_potion: {healing_potion()}")
+print()
